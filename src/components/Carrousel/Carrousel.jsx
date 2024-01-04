de30a1445d706140cdb920ebf10eebfae24ff55e
laukstl <laurent@kstl.fr>
2024-01-04 23:13:13 +0100

    Carrousel almost done ( commit for transfer )

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,28 +1,56 @@
 import './Carrousel.scss';
 import leftArrow from "../../assets/svg/leftArrow.svg";
 import rightArrow from "../../assets/svg/rightArrow.svg";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Carrousel ({pictures, title}) {
+    const numbersOfPictures = pictures.length;
+    const [index, setIndex] = useState(0);
+    const previousIndex = (index - 1 + numbersOfPictures) % numbersOfPictures;
+    const nextIndex = (index + 1) % numbersOfPictures;
+
     useEffect(() => {
-        // const index = 0;
-        const bannerImg = document.querySelector(".bannerImg");
+        const previousBannerImg = document.querySelector(".previous");
+        const actualBannerImg = document.querySelector(".actual");
+        const nextBannerImg = document.querySelector(".next");
+        
+        // const handleLeftTransitionEnd = () => {
+        //     setIndex((prevIndex) => (prevIndex - 1 + numbersOfPictures) % numbersOfPictures);
+        // };
+
+        const handleLeftArrowClick = () => {
+            previousBannerImg.classList.add("goLeft");
+            actualBannerImg.classList.add("goLeft");
+            nextBannerImg.classList.add("goLeft");
+
+            // previousBannerImg.addEventListener("transitionend", handleLeftTransitionEnd);
+            setIndex((prevIndex) => (prevIndex - 1 + numbersOfPictures) % numbersOfPictures);
+        };
 
         const leftArrowComponent = document.getElementById("leftarrow");
-        leftArrowComponent.addEventListener("click", () => {bannerImg.classList.add("goRight");});
+        leftArrowComponent.addEventListener("click", handleLeftArrowClick);
+
+        const handleRightArrowClick = () => {
+            previousBannerImg.classList.add("goRight");
+            actualBannerImg.classList.add("goRight");
+            nextBannerImg.classList.add("goRight");
+            setIndex((prevIndex) => (prevIndex + 1) % numbersOfPictures);
+        };
 
         const rightArrowComponent = document.getElementById("rightarrow");
-        rightArrowComponent.addEventListener("click", () => {bannerImg.classList.add("goLeft");});
+        rightArrowComponent.addEventListener("click", handleRightArrowClick);
 
         return () => { // lancé quand c'est unmount
-            leftArrowComponent.removeEventListener("click", () => {bannerImg.classList.add("goLeft");});
-            rightArrowComponent.removeEventListener("click", () => {bannerImg.classList.add("goRight");});
+            leftArrowComponent.removeEventListener("click", handleLeftArrowClick);
+            rightArrowComponent.removeEventListener("click", handleRightArrowClick);
         }
-    },[])
+    },[index, numbersOfPictures])
 
     return (
         <div className="carrousel">
-            <img className="bannerImg" src={pictures[0]} alt={title} />
+            <img className="bannerImg previous" src={pictures[previousIndex]} alt={title} />
+            <img className="bannerImg actual" src={pictures[index]} alt={title} />
+            <img className="bannerImg next" src={pictures[nextIndex]} alt={title} />
             <img className="arrow leftArrow" id="leftarrow" src={leftArrow} alt="Icon Back" />
             <img className="arrow rightArrow" id="rightarrow" src={rightArrow} alt="Icon Forward" />
         </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,14 @@ import logo from "../../assets/svg/logo_white.svg";
 import "./Footer.scss";
 
 function EnTravaux() {
-  return (
-    <footer>
-        <img className="footerLogo" src={logo} alt="Kasa Logo" />
-        <div className= "footerFont">@ 2020 Kasa. All rights reserved</div>
-    </footer>
-  )
+    return (
+        <div className="footerContainer">
+            <footer>
+                <img className="footerLogo" src={logo} alt="Kasa Logo" />
+                <div className="footerFont">@ 2020 Kasa. All rights reserved</div>
+            </footer>
+        </div>
+    )
 }
 
 export default EnTravaux
