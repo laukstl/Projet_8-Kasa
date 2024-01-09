5ea4aca5e88eead84188140a69dd5a4f5dab8920
laukstl <laurent@kstl.fr>
2024-01-09 01:35:43 +0100

    responsive on Error Page added

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,13 +2,18 @@ import { Link } from 'react-router-dom';
 import "./Error.scss";
 
 function EnTravaux() {
-  return (
-    <div className='errorPage'>
-      <div className="errorFont">404</div>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/">Retourner sur la page d'accueil</Link>
-    </div>
-  )
+    return (
+        <div className='errorPageContainer'>
+            <div className='errorPage'>
+                <div className="errorHTMLCodeFont">404</div>
+                <div className='errorMessage'>
+                    <span>Oups! La page que </span>
+                    <span>vous demandez n'existe pas.</span>
+                </div>
+                <Link to="/" className='errorPageHomeLink'>Retourner sur la page d'accueil</Link>
+            </div>
+        </div>
+    )
 }
 
 export default EnTravaux
