d734005589d5e055135175414abd38f8c661d700
LaucasZ <laucastell@gmail.com>
2024-01-12 16:51:27 +0100

    handle underline/nav with NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
-import { Link } from 'react-router-dom';
-import logo from "../../assets/svg/logo.svg";
 import "./Header.scss";
+import logo from "../../assets/svg/logo.svg";
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
-  return (
-    <div className='headerContainer'>
-        <header>
-          <img className="headerLogo" src={logo} alt="Kasa Logo" />
-          <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
-          </nav>
-        </header>
-    </div>
-  )
+    const links = [
+        {
+            txt: "Accueil",
+            to: "/"
+        },
+        {
+            txt: "A Propos",
+            to: "/about"
+        }
+    ]
+
+    return (
+        <div className='headerContainer'>
+            <header>
+                <img className="headerLogo" src={logo} alt="Kasa Logo" />
+                <nav>
+                    {
+                        links.map((item, index) => (
+                            <NavLink
+                                key={"NavLink"+index}
+                                className="links"
+                                to={item.to}
+                            >
+                                {item.txt}
+                            </NavLink>
+                        ))
+                    }
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default Header;
