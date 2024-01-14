4bfb826ce47523ce752bf343745b8c0d303766b1
laukstl <laurent@kstl.fr>
2024-01-14 22:33:58 +0100

    effect added on pictures carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -6,25 +6,41 @@ import { useState } from 'react';
 function Carrousel({ pictures, title }) {
     const numbersOfPictures = pictures.length;
     const [index, setIndex] = useState(0);
+    const [visible, setVisibility] = useState(true);
 
-    const handleLeftArrowClick = () => {
-        setIndex((prevIndex) => {
-            if (prevIndex === 0) {
-                return numbersOfPictures - 1;
-            }
-            return prevIndex - 1;
-        });
+    const handleLeftArrowClick = (event) => {
+        setVisibility(false);
+
+        setTimeout(() => {
+            setIndex((prevIndex) => {
+                if (prevIndex === 0) {
+                    return numbersOfPictures - 1;
+                }
+                return prevIndex - 1;
+            });
+            setVisibility(true);
+        }, 250);
     };
 
     const handleRightArrowClick = () => {
-        setIndex((prevIndex) => (prevIndex + 1) % numbersOfPictures);
+        setVisibility(false);
+
+        setTimeout(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % numbersOfPictures);
+            setVisibility(true);
+        }, 250);
     };
+
+    const checkPicturesCount = () => {
+        return numbersOfPictures < 2 ? 'hide' : ''
+    }
 
     return (
         <div className="carrousel">
-            <img className="bannerImg" src={pictures[index]} alt={title} />
-            <img className="arrow leftArrow" src={leftArrow} alt="Icon Back" onClick={handleLeftArrowClick} />
-            <img className="arrow rightArrow" src={rightArrow} alt="Icon Forward" onClick={handleRightArrowClick} />
+            <img className={`bannerImg ${visible ? 'visible noblur' : 'hidden blur'}`} src={pictures[index]} alt={title} />
+            <img className={`arrow leftArrow ${checkPicturesCount()}`} src={leftArrow} alt="Icon Back" onClick={handleLeftArrowClick} />
+            <img className={`arrow rightArrow ${checkPicturesCount()}`} src={rightArrow} alt="Icon Forward" onClick={handleRightArrowClick} />
+            <div className={`counter ${checkPicturesCount()}`}>{index+1}/{numbersOfPictures}</div>
         </div>
     );
 }
